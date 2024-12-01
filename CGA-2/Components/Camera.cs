1576c91005c2249f19bbbf57eae109bfa0dbefd9
Иван Сакун <reserv390@gmail.com>
2024-12-01 15:18:57 +0300

    Add Camera class

---
+++ CGA-2/Components/Camera.cs
@@ -0,0 +1,12 @@
+﻿using static System.Single;
+
+namespace CGA_2.Components
+{
+    public class Camera : SceneObject
+    {
+        public float FoV { get; set; } = Pi / 4f;
+        public float NearPlane { get; set; } = 0.1f;
+        public float FarPlane { get; set; } = 500f;
+        public float AspectRatio { get; set; }
+    }
+}
