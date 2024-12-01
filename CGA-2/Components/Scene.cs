241f8211cb98b71b44c1346894ace928d026dc00
Иван Сакун <reserv390@gmail.com>
2024-12-01 15:23:15 +0300

    Create scene structure

--- CGA-2/Components/Scene.cs
+++ CGA-2/Components/Scene.cs
@@ -1,7 +1,11 @@
-﻿namespace CGA_2.Components
+﻿using System.Windows.Media.Media3D;
+
+namespace CGA_2.Components
 {
     public class Scene
     {
-
+        public Camera Camera { get; set; } = new();
+        public List<Light> Lights { get; set; } = [];
+        public List<Model> Models { get; set; } = [];
     }
 }
