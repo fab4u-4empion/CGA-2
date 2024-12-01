e2139e4b7d725c6f98ed4e36032b7989cb5e7cfd
Иван Сакун <reserv390@gmail.com>
2024-12-01 12:48:08 +0300

    DPI settings

--- CGA-2/MainWindow.xaml.cs
+++ CGA-2/MainWindow.xaml.cs
@@ -11,14 +11,54 @@ using System.Windows.Shapes;
 
 namespace CGA_2
 {
+    using DPIScale = (double X, double Y);
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DPIScale Scale = (1, 1);
+
+        private WindowState LastState;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DpiScale dpi = VisualTreeHelper.GetDpi(this);
+            Scale = (dpi.DpiScaleX, dpi.DpiScaleY);
+
+            ResolutionTextBlock.Text = $"{(int)(Grid.ActualWidth * Scale.X)} × {(int)(Grid.ActualHeight * Scale.Y)}";
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key) 
+            {
+                case Key.F11:
+                    if (!e.IsRepeat)
+                    {
+                        if (WindowStyle != WindowStyle.None)
+                        {
+                            LastState = WindowState;
+                            WindowStyle = WindowStyle.None;
+                            ResizeMode = ResizeMode.NoResize;
+                            WindowState = WindowState.Normal;
+                            WindowState = WindowState.Maximized;
+                        }
+                        else
+                        {
+                            WindowStyle = WindowStyle.SingleBorderWindow;
+                            ResizeMode = ResizeMode.CanResize;
+                            WindowState = LastState;
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
